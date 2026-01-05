bd5466870815ac3ef8b0471539b9e98933cc162c
sonu44anand4597 <sonu444anand4597@gmail.com>
2026-01-05 19:02:50 +0000

    datatypes

---
+++ .devcontainer/01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS code as newer version
+
+//alert(3 + 3) // we are using node js , not browser
+
+console.log(3  
+    + 3) // code readibility should be high
+
+let name = "sonu"
+let age = 18
+let isloggedIn = false
+
+
+// number => 2 to the power of 53
+// bigint
+// string => ""
+// boolean => true/ false
+// null => standalone value
+// undefined =>  variable value not assigned
+// symbol => unique
+
+
+
+
+// object
+
+
+console.log(typeof "sonu");
+console.log(typeof "age");
+console.log(typeof undefined); // undefined
