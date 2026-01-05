ea859ab61458731a91851f5733e83c56aa4bd25f
sonu44anand4597 <sonu444anand4597@gmail.com>
2026-01-05 19:29:23 +0000

    conversion of datatypes

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,23 @@
+const accountId = 144553 
+let accountEmail = "hites@google.com"
+var accountPassword = "Delhi"
+
+
+
+
+// accountId = 2 // this is not allowed 
+
+accountEmail = "havgs@.com"
+accountPassword = "234567"
+accountCity = "bangalore"
+let accountState;
+
+console.log(accountId);
+
+
+
+/*
+prefer not to use var because of issue in bock scope and functional scope
+*/
+
+console.table([accountId, accountEmail , accountPassword, accountCity])
