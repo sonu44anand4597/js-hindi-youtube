5abdcabf22b5d878899fa75ab1441a5c9f3b1d37
sonu44anand4597 <sonu444anand4597@gmail.com>
2026-01-05 21:47:39 +0000

    data types summeries

---
+++ datatypes-summary.js
@@ -0,0 +1,40 @@
+// primitive 
+
+// 7 types: string ,nummber , Bollean , null , undefined , symbol , bigint
+
+
+const score = 100
+const scoreValue = 100.3
+
+
+const isLoggedIn = false
+const outsiddeTemp = null
+let userEmail;
+
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+
+
+
+// refrence (non primitive)
+
+
+// array , objects , functions
+
+
+const heros = [ "shaktiman", "naagraj", "dogs"]
+{
+
+    name: "sonu",
+    age: 22,
+}
+
+
+const myFunction = function(){
+    console.log("hello world")
+}
+
+
+console.log(typeof myFunction);
